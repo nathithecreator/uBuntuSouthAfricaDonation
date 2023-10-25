568a3c188161a9c6c0afa0a112462a69a1e15b78
Nkosinathi Sikhosana <ST10131426@rcconnect.edu.za>
2023-10-25 16:26:58 +0000

    updated add and edit

--- uBuntuSouthAfrica/Areas/Identity/Pages/Donates/EditDisaster.cshtml.cs
+++ uBuntuSouthAfrica/Areas/Identity/Pages/Donates/EditDisaster.cshtml.cs
@@ -1,24 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
 using System.Data.SqlClient;
 
 namespace uBuntuSouthAfrica.Pages.Donates
 {
     public class EditDisasterModel : PageModel
     {
-        public int NetAmount { get; set; } // Add the NetAmount property here
-
         public DisasterInfo disasterInfo = new DisasterInfo();
         public string errorMessage = "";
         public string successMessage = "";
 
         public void OnGet()
         {
-            String id = Request.Query["id"];
+            string id = Request.Query["id"];
 
             try
             {
-                String connectionString = "Server=tcp:djpromorosebank1.database.windows.net,1433;Initial Catalog=DJPromoWebApp;Persist Security Info=False;User ID=djnathi;Password=Mamabolo777;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Connection Timeout=30;";
+                string connectionString = "Server=tcp:djpromorosebank1.database.windows.net,1433;Initial Catalog=DJPromoWebApp;Persist Security Info=False;User ID=djnathi;Password=Mamabolo777;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Connection Timeout=30;";
                 SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder(connectionString);
                 builder.TrustServerCertificate = true;
                 connectionString = builder.ConnectionString;
@@ -26,26 +25,24 @@ namespace uBuntuSouthAfrica.Pages.Donates
                 using (SqlConnection connection = new SqlConnection(connectionString))
                 {
                     connection.Open();
-                    String sql = "SELECT * FROM Disasters WHERE id=@id";
+                    string sql = "SELECT * FROM Disasters WHERE ID=@id";
                     using (SqlCommand command = new SqlCommand(sql, connection))
                     {
-                        command.Parameters.AddWithValue("id", id);
+                        command.Parameters.AddWithValue("@id", id);
                         using (SqlDataReader reader = command.ExecuteReader())
                         {
                             if (reader.Read())
                             {
-                                disasterInfo.id = "" + reader.GetInt32(0).ToString();
+                                disasterInfo.id = reader.GetInt32(0).ToString();
                                 disasterInfo.Date = reader.GetDateTime(1).ToString();
                                 disasterInfo.DisasterName = reader.GetString(2);
                                 disasterInfo.Location = reader.GetString(3);
                                 disasterInfo.Description = reader.GetString(4);
                                 disasterInfo.DonationType = reader.GetString(5);
-
-    }
+                            }
                         }
                     }
                 }
-
             }
             catch (Exception ex)
             {
@@ -55,33 +52,29 @@ namespace uBuntuSouthAfrica.Pages.Donates
 
         public void OnPost()
         {
-            disasterInfo.id = Request.Query["id"];
-            disasterInfo.Date = Request.Form["name"];
+            disasterInfo.id = Request.Form["id"];
             disasterInfo.DisasterName = Request.Form["DisasterName"];
             disasterInfo.Location = Request.Form["Location"];
             disasterInfo.Description = Request.Form["Description"];
             disasterInfo.DonationType = Request.Form["DonationType"];
 
-
-            if (disasterInfo.DisasterName.Length == 0 ||
-                disasterInfo.Location.Length == 0 ||
-                disasterInfo.Description.Length == 0 ||  disasterInfo.DonationType.Length == 0)
+            if (string.IsNullOrEmpty(disasterInfo.DisasterName) || string.IsNullOrEmpty(disasterInfo.Location) || string.IsNullOrEmpty(disasterInfo.Description) || string.IsNullOrEmpty(disasterInfo.DonationType))
             {
-                errorMessage = "All the field are required";
+                errorMessage = "All the fields are required";
                 return;
             }
 
             try
             {
-                String connectionString = "Server=tcp:djpromorosebank1.database.windows.net,1433;Initial Catalog=DJPromoWebApp;Persist Security Info=False;User ID=djnathi;Password=Mamabolo777;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Connection Timeout=30;";
+                string connectionString = "Server=tcp:djpromorosebank1.database.windows.net,1433;Initial Catalog=DJPromoWebApp;Persist Security Info=False;User ID=djnathi;Password=Mamabolo777;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Connection Timeout=30;";
                 SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder(connectionString);
                 builder.TrustServerCertificate = true;
                 connectionString = builder.ConnectionString;
+
                 using (SqlConnection connection = new SqlConnection(connectionString))
                 {
                     connection.Open();
-                    String sql = "UPDATE Disasters SET DisasterName=@DisasterName, Location=@Location, Description=@Description, DonationType=@DonationType, Date=GETDATE() WHERE ID=@id";
-
+                    string sql = "UPDATE Disasters SET DisasterName=@DisasterName, Location=@Location, Description=@Description, DonationType=@DonationType WHERE ID=@id";
 
                     using (SqlCommand command = new SqlCommand(sql, connection))
                     {
@@ -91,11 +84,9 @@ namespace uBuntuSouthAfrica.Pages.Donates
                         command.Parameters.AddWithValue("@Description", disasterInfo.Description);
                         command.Parameters.AddWithValue("@DonationType", disasterInfo.DonationType);
 
-                     
-        command.ExecuteNonQuery();
+                        command.ExecuteNonQuery();
                     }
                 }
-
             }
             catch (Exception ex)
             {
@@ -104,8 +95,6 @@ namespace uBuntuSouthAfrica.Pages.Donates
             }
 
             Response.Redirect("/Identity/Donates/DisasterIndex");
-
-
         }
     }
 }
